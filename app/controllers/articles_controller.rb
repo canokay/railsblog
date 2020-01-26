faa98f0ff76296c5eb20ac13cb52492b471a81d7
Can Okay <canokay2014@gmail.com>
2020-01-26 15:46:43 +0300

    ArticlesController before_action re-added.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
     @articles = Article.all
@@ -10,11 +11,9 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
 
@@ -29,7 +28,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       redirect_to @article
     else
@@ -38,7 +36,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-  @article = Article.find(params[:id])
   @article.destroy
 
   redirect_to articles_path
@@ -48,6 +45,10 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:title, :text)
     end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
 
 end

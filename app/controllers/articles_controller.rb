092ac067cb877ca3c6fd7ab452acfd370226e0b5
Can Okay <canokay2014@gmail.com>
2020-01-26 15:24:00 +0300

    before_action delted.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
-  before_action : set_article,only : [:show, :edit, :update, :destroy]
+
+
   def index
     @articles = Article.all
   end
@@ -9,9 +10,11 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
 
@@ -26,7 +29,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-
+    @article = Article.find(params[:id])
     if @article.update(article_params)
       redirect_to @article
     else
@@ -35,6 +38,7 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
+  @article = Article.find(params[:id])
   @article.destroy
 
   redirect_to articles_path
@@ -45,8 +49,5 @@ class ArticlesController < ApplicationController
       params.require(:article).permit(:title, :text)
     end
 
-    def set_article
-      @article = Article.find(params[:id])
-    end
 
 end

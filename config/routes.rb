b2ef8d7b44a582b6c573167ec9a5c38afeae9486
Can Okay <canokay2014@gmail.com>
2020-01-26 10:49:13 +0300

    resources :articles routes added.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get 'welcome/Index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :articles
+
   root 'welcome#Index'
+
+
+
+
 end
